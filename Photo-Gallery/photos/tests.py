773c19fd2a305d2e8e81d0b1c1cb6358de0513b6
martinsmandina <martinsmandina@gmail.com>
2020-11-15 13:22:40 +0300

    create category test class

--- Photo-Gallery/photos/tests.py
+++ Photo-Gallery/photos/tests.py
@@ -5,6 +5,7 @@ from .models import Location
 class LocationTestClass(TestCase):
     def setUp(self):
         self.location = Location(title = 'Mombasa')
+        self.location.save()
   
     def test_instance(self):
         self.assertTrue(isinstance(self.location,Location)) 
@@ -13,3 +14,22 @@ class LocationTestClass(TestCase):
         self.location.location_save()
         locations = Location.objects.all()
         self.assertTrue(len(locations)>0)
+
+    def test_location_delete(self):
+        self.location.location_delete()
+        location = Location.objects.all()
+        self.assertTrue(len(location)==0)
+
+class CategoryTestClass(TestCase):
+    def setup(self):
+        self.category = Category(title = 'nature')
+        self.category.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category)) 
+
+        
+
+
+    
+

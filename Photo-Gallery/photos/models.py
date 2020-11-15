fac3070e6751cdfd6eb66977681379682d0fa626
martinsmandina <martinsmandina@gmail.com>
2020-11-15 12:30:24 +0300

    create class category

--- Photo-Gallery/photos/models.py
+++ Photo-Gallery/photos/models.py
@@ -13,4 +13,11 @@ class Location(models.Model):
     def location_delete(self):
         self.delete()
 
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    
 

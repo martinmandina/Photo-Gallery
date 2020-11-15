b70ed75a828563e4603090838223c9c773fbf7ef
martinsmandina <martinsmandina@gmail.com>
2020-11-15 11:46:18 +0300

    add a gitignore

--- Photo-Gallery/gallery/settings.py
+++ Photo-Gallery/gallery/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+          'USER': 'martinmandina',
+    'PASSWORD':'alicewambui',
+
     }
 }
 

d5948dbdf2bf502b998bd63cf765e8c3511346ef
martinsmandina <martinsmandina@gmail.com>
2020-11-15 12:47:02 +0300

    ran test for location

--- Photo-Gallery/photos/models.py
+++ Photo-Gallery/photos/models.py
@@ -13,11 +13,14 @@ class Location(models.Model):
     def location_delete(self):
         self.delete()
 
-class Category(models.Model):
-    title = models.CharField(max_length=50)
+# class Category(models.Model):
+#     title = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
 
-    
+#     def category_save(self):
+#         self.save()
 
+#     def category_delete(self):
+#         self.delete()
--- Photo-Gallery/photos/tests.py
+++ Photo-Gallery/photos/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Location
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(title = 'Mombasa')
+  
+
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))   

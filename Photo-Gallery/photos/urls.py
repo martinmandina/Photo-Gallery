18a17ba2ba5c83ad5a14b80531157fbeefdc441d
martinsmandina <martinsmandina@gmail.com>
2020-11-15 19:51:35 +0300

    create search view

--- Photo-Gallery/photos/urls.py
+++ Photo-Gallery/photos/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.display_images,name='photos'),
-    # path('photo/(\d+)',views.image_one, name='singleimage'),
+    path('search/', views.search_results,name='search_results'),
+    path('photo/(\d+)',views.image_single, name='single_image'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- Photo-Gallery/photos/views.py
+++ Photo-Gallery/photos/views.py
@@ -8,4 +8,18 @@ def display_images(request):
     images = Image.get_images()
     return render(request,'images.html',{'images':images})
 
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = images.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"image": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
 
